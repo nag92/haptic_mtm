58d0c906a3ddb60fa927b6e0ec348bd6fb164e69
Vignesh <vigneshvarier@gmail.com>
2020-02-26 15:53:14 -0500

    added round off function edited

--- PSM/parser.py
+++ PSM/parser.py
@@ -3,15 +3,10 @@ import rospy
 import numpy as np
 import csv
 import math
-import matplotlib.pyplot as plt
-<<<<<<< HEAD
 from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
 
 
-=======
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
->>>>>>> 17d2247177e1d7751b5aadf4a43e5030a1ad051b
 def getData(bag, topic):
     """
     Extract a pos from the a topic
@@ -29,7 +24,6 @@ def getData(bag, topic):
     idx = 0
 
     for top, msg, t in cur_bag.read_messages(topics=[topic],
-                                             start_time=t_start,
                                              start_time=t_start,
                                              end_time=t_end):
         cur = np.array([msg.pose.position.x, msg.pose.position.y, msg.pose.position.z])
@@ -92,20 +86,14 @@ if __name__ == "__main__":
     file_name = "/home/vignesh/Thesis_Suture_data/trial2/suture_data_trial2/781266"
     data = getData(file, topic)
     rnd, steps = make_steps(data)
-<<<<<<< HEAD
     make_file(rnd, steps, file_name)
-=======
->>>>>>> 17d2247177e1d7751b5aadf4a43e5030a1ad051b
 
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
 
-<<<<<<< HEAD
     ax.plot(rnd[:, 0], rnd[:, 1], rnd[:, 2])
     plt.savefig('/home/vignesh/Thesis_Suture_data/trial2/suture_data_trial2/781266.png')
     plt.show()
-=======
     ax.plot(rnd[:,0], rnd[:,1], rnd[:,2])
     plt.show()
     #make_file(rnd, steps, file_name)
->>>>>>> 17d2247177e1d7751b5aadf4a43e5030a1ad051b
